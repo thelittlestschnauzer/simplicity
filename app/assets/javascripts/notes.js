967c5881436ae9a5a7b21cce71102db166b59826
github username <github email address>
2018-08-16 11:13:40 -0400

    added some javascript to the notes index page

---
+++ app/assets/javascripts/notes.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('turbolinks:load', function () {
+  // the elements had to be assigned into variables
+  const openNoteButton = document.getElementById('new-note-button')
+  const notePopover = document.getElementById('new-note-popover')
+  // this will check to see if the elements even exist. If they don't a warning will populate. (hopefully)
+  if (openNoteButton && notePopover) {
+    openNoteButton.addEventListener('click', function () {
+      // if the note popover class list contains the 'is-hidden' class, remove it, otherwise return null
+      return notePopover.classList.contains('is-hidden') ? notePopover.classList.remove('is-hidden') : null
+    }, false)
+
+    const cancelNotePopover = document.getElementById('cancel-note-popover')
+
+    cancelNotePopover.addEventListener('click', function () {
+
+      return notePopover.classList.add('is-hidden')
+    }, false)
+  }
+})
